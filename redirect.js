d2cd606b4886eb5a4804840b5fc5ba0ffab3cc86
redtubebzz <parvesads@gmail.com>
2026-04-18 12:28:01 +0600

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
-const buttonRedirectURL = "https://villagetraitgarret.com/fz3ifw9n?key=255cb9b54aebe1dca0f89408b47e3fcf";
+const buttonRedirectURL = "https://motortape.com/esw47ptigs?key=e50a789fc06e9d76e6b0efba41dc2c0e";
 const pageRedirectURL = "https://pairturnnumerous.com/rze4wd8aa?key=74d74ecf1cacbe96436ceb718716741c";
-const fallbackURL = "https://villagetraitgarret.com/fz3ifw9n?key=255cb9b54aebe1dca0f89408b47e3fcf";
+const fallbackURL = "https://motortape.com/esw47ptigs?key=e50a789fc06e9d76e6b0efba41dc2c0e";
 
 function handleRedirect(e) {
   document.removeEventListener('click', handleRedirect);
